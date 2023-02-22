35b42ea286b71c0f0a5476d0f9c65ca6b56fbd7d
AlexLineZ <alialex58@mail.ru>
2023-02-22 20:19:59 +0700

    implemented counting

--- app/src/main/java/com/example/calculator/activities/MainActivity.kt
+++ app/src/main/java/com/example/calculator/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculator.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
@@ -10,11 +11,12 @@ open class MainActivity : AppCompatActivity() {
     var lastOperation = false
     var isNew = true
     var previousValue = ""
+    var typeOfOperation = ""
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.textResult.text = previousValue
 
         binding.button0.setOnClickListener{setText("0")}
         binding.button1.setOnClickListener{setText("1")}
@@ -26,25 +28,57 @@ open class MainActivity : AppCompatActivity() {
         binding.button7.setOnClickListener{setText("7")}
         binding.button8.setOnClickListener{setText("8")}
         binding.button9.setOnClickListener{setText("9")}
-        binding.buttonPlus.setOnClickListener{setOperation("+")}
-        binding.buttonMinus.setOnClickListener{setOperation("-")}
-        binding.buttonMultiply.setOnClickListener{setOperation("×")}
-        binding.buttonDevide.setOnClickListener{setOperation("/")}
+
+        binding.buttonPlus.setOnClickListener{
+            if (previousValue.isNotEmpty() && typeOfOperation.isNotEmpty()){
+                clickEqualOrOperation()
+            }
+            else {
+                setOperation("+")
+            }
+        }
+        binding.buttonMinus.setOnClickListener{
+            if (previousValue.isNotEmpty() && typeOfOperation.isNotEmpty()){
+                clickEqualOrOperation()
+            }
+            else {
+                setOperation("-")
+            }
+        }
+        binding.buttonMultiply.setOnClickListener{
+            if (previousValue.isNotEmpty() && typeOfOperation.isNotEmpty()){
+                clickEqualOrOperation()
+            }
+            else {
+                setOperation("×")
+            }
+        }
+        binding.buttonDevide.setOnClickListener{
+            if (previousValue.isNotEmpty() && typeOfOperation.isNotEmpty()){
+                clickEqualOrOperation()
+            }
+            else {
+                setOperation("/")
+            }
+        }
         binding.buttonPlusMinus.setOnClickListener{setMinusOrPlus()}
         binding.buttonAC.setOnClickListener{deleteText()}
         binding.buttonDelete.setOnClickListener { deleteLastSymbol() }
+        binding.buttonEquals.setOnClickListener{clickEqualOrOperation()}
     }
 
     private fun setOperation(str: String){
-        println(lastOperation)
         if (lastOperation){
             val newText = binding.textScreen.text.toString()
             binding.textScreen.text = newText.substring(0, newText.length - 1)
             binding.textScreen.append(str)
+            typeOfOperation = str
         }
 
-        if (!binding.textScreen.text.toString().isNullOrEmpty() && !lastOperation){
+        if (binding.textScreen.text.toString().isNotEmpty() && !lastOperation){
+            previousValue = binding.textScreen.text.toString()
             binding.textScreen.append(str)
+            typeOfOperation = str
             isNew = true
             lastOperation = true
         }
@@ -59,6 +93,7 @@ open class MainActivity : AppCompatActivity() {
         lastOperation = false
     }
 
+    @SuppressLint("SetTextI18n")
     private fun setMinusOrPlus() {
         if (binding.textScreen.text.isNullOrEmpty()){
             return
@@ -76,16 +111,61 @@ open class MainActivity : AppCompatActivity() {
             lastOperation = false
         }
         binding.textScreen.text = "0"
+        previousValue = ""
+        typeOfOperation = ""
         isNew = true
     }
 
     private fun deleteLastSymbol() {
-        var newText = binding.textScreen.text.toString()
-        if (!newText.isNullOrEmpty()){
+        val newText = binding.textScreen.text.toString()
+        if (newText == "Error"){
+            deleteText()
+        }
+        else if (newText.isNotEmpty()){
             binding.textScreen.text = newText.substring(0, newText.length - 1)
             if (lastOperation){
                 lastOperation = false
+                typeOfOperation = ""
             }
         }
     }
+
+    fun setError(){
+        binding.textScreen.text = "Error"
+        lastOperation = false
+        typeOfOperation = ""
+        previousValue = ""
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun clickEqualOrOperation() {
+        var nowValue = binding.textScreen.text.toString()
+        if (nowValue[nowValue.length - 1] == '+'
+            || nowValue[nowValue.length - 1] == '-'
+            || nowValue[nowValue.length - 1] == '×'
+            || nowValue[nowValue.length - 1] == '/'){
+            setError()
+        }
+        when(typeOfOperation){
+            "+" -> binding.textScreen.text = formatNumber(previousValue.toDouble() + nowValue.toDouble())
+            "-" -> binding.textScreen.text = formatNumber(previousValue.toDouble() - nowValue.toDouble())
+            "×" -> binding.textScreen.text = formatNumber(previousValue.toDouble() * nowValue.toDouble())
+            "/" -> binding.textScreen.text = formatNumber(previousValue.toDouble() / nowValue.toDouble())
+        }
+
+        previousValue = nowValue
+        typeOfOperation = ""
+    }
+
+    fun formatNumber(num: Double): String {
+        if (num == Double.POSITIVE_INFINITY || num == Double.NEGATIVE_INFINITY){
+            return "Error"
+        }
+        return if (num == num.toInt().toDouble()) {
+            num.toInt().toString()
+        } else {
+            num.toString()
+        }
+    }
+
 }
